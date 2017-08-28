ab889b52afc70b6c15ca5538f0fcf39c30ef0d51
clarisssa <clarissaparo@gmail.com>
2017-08-28 17:27:34 -0300

    buscador de pelicula

---
+++ movies-app/src/app/app.search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './app.search.component.html',
+  styleUrls: ['./app.search.component.css']
+})
+
+export class SearchComponent {
+ text: string;
+}

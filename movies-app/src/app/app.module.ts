9b7365aba29f81607b261fb1bf4beb1297bfa8dd
ceciliaalvarez <chechu.rcentral@gmail.com>
2017-08-31 14:49:38 -0300

    correccion del footer y llamada a la api

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app.header.component';
 import { AppFooterComponent } from './app.footer.component';
 import { AppBodyComponent } from './app.body.component';
-import { MovieComponent} from './app.movie.component';
-import {SearchComponent} from './app.search.component';
+import { MovieComponent } from './app.movie.component';
+import { SearchComponent } from './app.search.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {SearchComponent} from './app.search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]

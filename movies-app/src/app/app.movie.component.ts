601782344eef6df5cda4fb49456061a2ce422e14
ceciliaalvarez <chechu.rcentral@gmail.com>
2017-08-31 17:26:14 -0300

    hay que borrarlo

--- movies-app/src/app/app.movie.component.ts
+++ movies-app/src/app/app.movie.component.ts
@@ -13,12 +13,22 @@ export class MovieComponent {
   public title: string;
   private poster ;
   private overview ;
-  private score ;
+  private vote_average ;
   private id ;
+  private poster_path;
+  private release_date: Date;
 
   constructor(http: Http) {
     http.get('http://localhost:9000/api/movies/info/278')
       .map(res => res.json())
-      .subscribe(movie => this.title = movie.title);
+      .subscribe(movie => {
+                            this.title = movie.title;
+                            this.id = movie.id;
+                            this.overview = movie.overview;
+                            this.vote_average = movie.vote_average;
+                            this.poster_path = movie.poster_path;
+                            this.release_date = movie.release_date;
+                          }
+                );
    }
 }
